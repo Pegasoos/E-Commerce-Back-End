e7835106d35d001e61d7c7d538fdcf90157c927d
Pegasoos <circumambulation24@yahoo.com>
2021-11-17 00:35:21 -0500

    Category/Product Sequelize relationship coded

--- models/index.js
+++ models/index.js
@@ -4,10 +4,12 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
-
-// Categories have many Products
-
+Product.belongsTo(Category,{
+  foreignKey: 'category_id'
+})
+Category.hasMany(Product,{
+  foreignKey: 'category_id'
+})
 // Products belongToMany Tags (through ProductTag)
 
 // Tags belongToMany Products (through ProductTag)
