b0ced5dd648aeb22b25253f9c88f5dd78e35c4c3
Pegasoos <circumambulation24@yahoo.com>
2021-11-16 23:04:19 -0500

    Defined Category Model

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -5,9 +5,8 @@ const sequelize = require('../config/connection');
 class ProductTag extends Model {}
 
 ProductTag.init(
-  {
-    // define columns
-  },
+  
+
   {
     sequelize,
     timestamps: false,
