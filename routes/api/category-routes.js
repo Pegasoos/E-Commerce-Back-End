9a95ba0bc860c454d9dc487be5c395a6cf31c1d0
Pegasoos <circumambulation24@yahoo.com>
2021-11-17 22:56:15 -0500

    Category Post Request Functional

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -26,12 +26,16 @@ router.get('/:id', async (req, res) => {
   catch(err){
     res.status(500).json(err);
   }
-  // find one category by its `id` value
-  // be sure to include its associated Products
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post('/', async (req, res) => {
+  try{
+    const newCategoryData = await Category.create(req.body);
+    res.status(200).json(newCategoryData)
+  }
+  catch(err){
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
