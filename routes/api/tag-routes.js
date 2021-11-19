b21b7c92a7282d4107f992d5385c4da701736155
Pegasoos <circumambulation24@yahoo.com>
2021-11-19 08:42:50 -0500

    Tag POST Route Functional

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -28,6 +28,13 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
   // create a new tag
+    try{
+      const newTagData = await Tag.create(req.body);
+      res.status(200).json(newTagData)
+    }
+    catch(err){
+      res.status(400).json(err);
+    }
 });
 
 router.put('/:id', async (req, res) => {
