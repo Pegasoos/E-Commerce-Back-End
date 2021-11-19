6a56d11f18f5dae616f7a0ab57a68899097a3e1b
Pegasoos <circumambulation24@yahoo.com>
2021-11-18 21:56:53 -0500

    Category Update Route Complete

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -38,8 +38,22 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.put('/:id', async (req, res) => {
+  try{
+    const categoryData = await Category.update({category_name: req.body.category_name}, {
+      where:{
+        id: req.params.id,
+      }
+    })
+    if(!categoryData[0]){
+      res.status(404).json({req})
+      return
+    }
+    res.status(200).json(categoryData);
+  }
+  catch(err){
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
