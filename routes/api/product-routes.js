d3f74bd4864224b5ef847635d3955dcbd2f6d7e9
Pegasoos <circumambulation24@yahoo.com>
2021-11-18 22:53:41 -0500

    Get Product include Category Route Functional

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,8 +4,16 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
+router.get('/', async (req, res) => {
+  try{
+    const categoryData = await Product.findAll({
+      include: Category
+    });
+    res.status(200).json(categoryData);
+    }
+    catch (err){
+      res.status(500).json(err);
+    }
   // be sure to include its associated Category and Tag data
 });
 
